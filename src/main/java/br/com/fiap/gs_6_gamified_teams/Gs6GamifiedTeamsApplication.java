dd5a9ae394c72ff5265db71472fcd9cc178c3ab2
Rick Alves Domingues <rickpoterdom@gmail.com>
2025-11-13 13:18:44 -0300

    first commit

---
+++ src/main/java/br/com/fiap/gs_6_gamified_teams/Gs6GamifiedTeamsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gs_6_gamified_teams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gs6GamifiedTeamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gs6GamifiedTeamsApplication.class, args);
+	}
+
+}
